f00e594cf2f9ef3df7c32af9177def8554f3131c
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-02-01 15:08:14 +0300

    Slider update css

--- components/Slider/Slider.tsx
+++ components/Slider/Slider.tsx
@@ -45,12 +45,12 @@ const Slider: React.FC = () => {
       <Head>
         <title>Slider</title>
       </Head>
-      <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div className="w-full  relative group">
         <div
           style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
           className="w-full h-[400px] rounded-2xl bg-center bg-cover duration-500"
         >
-          <div className="w-[426px] h-[228px] grid place-content-center ml-16">
+          <div className="w-[426px] h-[228px] grid place-content-center ml-20">
             <label className="text-[#F0B861] font-manrope font-bold text-5xl">
               25% discount
             </label>
@@ -62,11 +62,11 @@ const Slider: React.FC = () => {
           </div>
         </div>
 
-        <div className="hidden group-hover:block absolute top-[35%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactLeft onClick={prevSlide} size={30} />
         </div>
 
-        <div className="hidden group-hover:block absolute top-[35%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <div className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
         <div className="flex top-4 justify-center py-2">
