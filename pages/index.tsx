cd68bd1e668f5deecd497aefb424d237326845db
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-02-01 15:08:45 +0300

    Add home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "@next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import Head from 'next/head'
+import { useEffect, useState } from 'react'
+import Header from '@/components/Header/Header'
+import Slider from '@/components/Slider/Slider'
+import ProductCatalogList from '@/components/Product/ProductCatalogList/ProductCatalogList'
+import { getCategories } from '@/services/ProductService'
+import { Category } from '@/types'
 
 export default function Home() {
+  const [categories, setCategories] = useState<Category[]>([])
+  const getAllCategories = async () => {
+    const result = await getCategories()
+
+    setCategories(result.category)
+  }
+  useEffect(() => {
+    getAllCategories()
+  }, [])
+
   return (
     <>
       <Head>
@@ -14,7 +25,18 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>initial</div>
+
+      <Header />
+      <div className="mx-auto max-w-screen-2xl px-4 sm:py-5 sm:px-6 lg:max-w-screen-2xl lg:px-8">
+        <Slider />
+        {categories.map((category, index) => (
+          <ProductCatalogList
+            key={index}
+            title={category.name}
+            id={category.id}
+          />
+        ))}
+      </div>
     </>
-  );
+  )
 }
