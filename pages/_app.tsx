66a2eb9d01de5a18acec7c82949fd1d3a38d4f52
MuhammetLvnt <79282877+MuhammetLvnt@users.noreply.github.com>
2023-02-02 04:05:37 +0300

    remove

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,10 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import store from '../store'
-import Authentication from '@/components/Auth/Authentication'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Authentication />
       <Component {...pageProps} />
     </Provider>
   )
